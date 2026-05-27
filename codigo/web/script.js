774cac2fd41e94fccb3aee578d1c5efe94e09c55
Joao Emannoel <joaoemannoel7@gmail.com>
2026-05-27 14:58:01 -0300

    Alterar estilo visual das páginas e menu

--- codigo/web/script.js
+++ codigo/web/script.js
@@ -1,23 +1,19 @@
+
 function gerarEnergia(){
 
-let energia =
-Math.floor(Math.random() * 100);
+    let energia = Math.floor(Math.random() * 100);
 
-document.getElementById("energia")
-.innerHTML = energia + "%";
+    document.getElementById("energia").innerHTML = energia + "%";
 
-if(energia >= 50){
+    if(energia >= 50){
 
-document.getElementById("status")
-.innerHTML =
-"Energia Solar Estável";
+        document.getElementById("status").innerHTML =
+        "Energia Solar Estável";
 
-}else{
+    }else{
 
-document.getElementById("status")
-.innerHTML =
-"Baixa geração de energia";
+        document.getElementById("status").innerHTML =
+        "Baixa geração de energia";
 
-}
-
+    }
 }
